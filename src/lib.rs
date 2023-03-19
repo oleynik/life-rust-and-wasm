3c6aa10f62ec8e716ab750654a01ecc5a5dd821b
Alex Oliinyk <alex.p.oliinyk@gmail.com>
2023-03-19 19:44:54 +0100

    Implemented the Game.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod utils;
 
+use std::fmt;
+use std::fmt::Formatter;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -9,12 +11,109 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    let string = format!("Hello, {}!", String::from(name));
-    alert(string.as_str());
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+impl Universe {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut result = 0;
+        for delta_row in [self.height - 1, 0, 1] {
+            for delta_column in [self.width - 1, 0, 1] {
+                if delta_row == 0 && delta_column == 0 {
+                    continue;
+                }
+                let current_row = (row + delta_row) % self.height;
+                let current_column = (column + delta_column) % self.width;
+                let idx = self.get_index(current_row, current_column);
+                result += self.cells[idx] as u8;
+            }
+        }
+        result
+    }
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn new() -> Self {
+        let width = 64;
+        let height = 64;
+
+        let cells = (0..width * height)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 || i % 13 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+        for i in 0..self.height {
+            for j in 0..self.width {
+                let neighbors = self.live_neighbor_count(i, j);
+                let idx = self.get_index(i, j);
+                let current = self.cells[idx];
+                next[idx] = match (current, neighbors) {
+                    (Cell::Alive, 2 | 3) => Cell::Alive,
+                    (Cell::Alive, _) => Cell::Dead,
+                    (Cell::Dead, 3) => Cell::Alive,
+                    (c, _) => c,
+                }
+            }
+        }
+        self.cells = next;
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
+    }
 }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,78 @@
-import * as wasm from "life";
+import {Universe, Cell} from "life";
+import {memory} from "life/life_bg";
 
-wasm.greet("Alex");
+const CELL_SIZE = 10; // px
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+
+const universe = Universe.new();
+const height = universe.height();
+const width = universe.width();
+
+const canvas = document.getElementById("canvas");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const context = canvas.getContext('2d');
+
+const drawGrid = () => {
+  context.beginPath();
+  context.strokeStyle = GRID_COLOR;
+
+  // Vertical lines.
+  for (let i = 0; i <= width; i++) {
+    context.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    context.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  // Horizontal lines.
+  for (let j = 0; j <= height; j++) {
+    context.moveTo(0,                           j * (CELL_SIZE + 1) + 1);
+    context.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+  }
+
+  context.stroke();
+};
+
+const getIndex = (row, column) => {
+  return row * width + column;
+};
+
+const drawCells = () => {
+  const cellsPtr = universe.cells();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+  context.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      context.fillStyle = cells[idx] === Cell.Dead
+          ? DEAD_COLOR
+          : ALIVE_COLOR;
+
+      context.fillRect(
+          col * (CELL_SIZE + 1) + 1,
+          row * (CELL_SIZE + 1) + 1,
+          CELL_SIZE,
+          CELL_SIZE
+      );
+    }
+  }
+
+  context.stroke();
+};
+
+const renderLook = () => {
+  universe.tick();
+  drawGrid();
+  drawCells();
+
+  requestAnimationFrame(renderLook);
+};
+
+drawGrid();
+drawCells();
+requestAnimationFrame(renderLook);
